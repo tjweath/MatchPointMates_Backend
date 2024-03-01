aedcd93caf518a744fcb02cfb0e60fdea3fa05fe
tjweath <t_weatherseed@hotmail.com>
2024-03-01 12:26:40 +0000

    final push

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Player(models.Model):
-    # Assuming player_id is an auto-generated primary key, we'll use AutoField
     player_name = models.CharField(max_length=50)
     player_country = models.CharField(max_length=30)
 
